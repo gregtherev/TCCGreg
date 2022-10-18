3aba53e532801667ad25d1f7b4992085a47de405
Genese Lessa <genese.lessa@gmail.com>
2022-10-18 11:40:12 -0300

    some adjusts for authentication

--- applications/accounts/views.py
+++ applications/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login_view(request):
+    # TODO - change to a success page
+    success_url = 'semantic_testing'
+
+    if request.user.is_authenticated:
+        return redirect(success_url)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect(success_url)
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
--- rank/urls.py
+++ rank/urls.py
@@ -6,6 +6,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path
 
+from applications.accounts.views import login_view, logout_view
 # just for semantic-ui and vuejs test
 from django.views.generic import TemplateView
 
@@ -17,7 +18,6 @@ urlpatterns = [
          TemplateView.as_view(template_name='checking_semantic.html'),
          name='semantic_testing'),
 
-    path('login/',
-         TemplateView.as_view(template_name='login.html'),
-         name='semantic_login'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
