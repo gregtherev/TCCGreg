3d0eec57c0af31d964f25d7358617493c85a5d04
Genese Lessa <genese.lessa@gmail.com>
2022-10-18 11:56:29 -0300

    session applied to test page

--- rank/urls.py
+++ rank/urls.py
@@ -6,18 +6,16 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path
 
+from rank.base_views import test_page_for_templates
+
 from applications.accounts.views import login_view, logout_view
-# just for semantic-ui and vuejs test
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # semantic-ui testing
-    path('ranking/',
-         TemplateView.as_view(template_name='checking_semantic.html'),
-         name='semantic_testing'),
+    path('ranking/', test_page_for_templates, name='semantic_testing'),
 
-    path('login/', login_view, name='login'),
+    path('', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
 ]
