a66cc5b798fa57f4d810085ae1289317049695b3
Genese Lessa <genese.lessa@gmail.com>
2022-11-05 23:29:42 -0300

    added components to rank screen

--- rank/base_views.py
+++ rank/base_views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def test_page_for_templates(request):
-    return render(request, 'checking_semantic.html')
+    return render(request, 'base_leaderboard.html')
