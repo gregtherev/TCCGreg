4aaf33372d913c82788c40855009c10a5e8636a2
Guilherme Greg <gregui3108@hotmail.com>
2023-02-15 21:57:54 -0300

    linking left and right header components to the api using axios

--- applications/events/api.py
+++ applications/events/api.py
@@ -1,5 +1,8 @@
-"""This module contains the API for the accounts application."""
+"""This module contains the API for the events application."""
 from ninja import Router
+from datetime import timedelta, datetime, date
+
+from ..events.models import Event
 
 router = Router()
 
@@ -7,3 +10,30 @@ router = Router()
 @router.get("/hello")
 def hello(request):
     return "Hello, World!"
+
+
+@router.get("/{event_id}")
+def event_info(request, event_id: int):
+    event = Event.objects.get(pk=event_id)
+    event_dict = {
+        "event_name": event.name,
+        "event_institution": event.institution.name,
+    }
+
+    return event_dict
+
+
+@router.get("/event_duration/{event_id}")
+def event_duration(request, event_id: int):
+    event = Event.objects.get(pk=event_id)
+    remaining_time = (datetime.combine(date.today(), event.start_time)
+                      + timedelta(hours=event.duration))
+    now = datetime.now()
+    remaining_seconds = (remaining_time-now).total_seconds()
+
+    event_dict = {
+        "date": event.date.strftime("%d/%m/%Y"),
+        "remaining_seconds": int(remaining_seconds)
+    }
+
+    return event_dict
